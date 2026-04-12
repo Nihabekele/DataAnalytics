1f88592d05d198889694cff24747fb1bb9efd73f
Nihabekele <linabekele12@gmail.com>
2026-04-11 20:57:16 -0400

    Added queries of actor and film tables

--- week-02/Ex1B_first_query.sql
+++ week-02/Ex1B_first_query.sql
@@ -1,3 +1,7 @@
 /*
 Sakila is a database used to collect information about film rentals and inventory for use by DVD rental store chain.
 */
+USE sakila;
+SELECT * FROM actor; -- Retrieved 200 records
+
+SELECT * FROM film; -- Retrieved 1000 records
