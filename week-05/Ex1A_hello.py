985a57b30d26d97f5b0defd7cc8c909445d24ce6
Nihabekele <linabekele12@gmail.com>
2026-05-02 23:15:00 -0400

    Add Exercise1A to week-05

--- week-05/Ex1A_hello.py
+++ week-05/Ex1A_hello.py
@@ -1,1 +1,26 @@
 print('Hello world!')
+
+message = 'Hello World!'
+print(message)
+
+# "Hello world" print twice because there are two print commands in the code.
+
+
+# Displaying dollars and cents
+
+dollars = 3
+cents = .50
+print(dollars + cents)
+
+# I notice that the result is 3.5 which is a decimal (float). 
+# Python combined the whole number and the decimal together.
+
+
+cents = cents + .25
+print(dollars + cents)
+
+
+d_str = '3 dollars'
+c_str = '50 cents'
+
+print(d_str + " " + c_str)
