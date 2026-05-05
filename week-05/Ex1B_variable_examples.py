abf97ff7fed323d71ff05d02bacd12eb0e494eda
Nihabekele <linabekele12@gmail.com>
2026-05-04 22:43:34 -0400

    "Add Exercise1B to week-05"

---
+++ week-05/Ex1B_variable_examples.py
@@ -0,0 +1,51 @@
+# Lab 1
+
+# Question 2 & 3 Variables with Assumptions
+
+customer_id = "C-1221" # I used a string because some ID have letters and dashes
+
+customer_name = "Emily Jone" # Assuming one variable is enough but separate first and last name might be better for sorting
+
+customer_gender = "Female" # I wrote the full word is clearer than just F
+
+customer_dob = "01/01/1978" # I'm assuming this is Month/Day/Year format 
+
+license_number = "VA-1234567" # This needs to be a string because it contains a letter
+
+policy_number = "9876543" # Assuming this is just a label, so I treated it as a string instead of a number
+
+# Question 4  Personal Variables
+
+my_name = "Hana Merawi" # I wrote my first and last name together 
+
+my_hometown = "Addis Ababa, Ethiopia" # Assuming city and country together provides the best context
+
+print(customer_name)
+
+print(customer_id)
+
+print(my_name)
+
+print(my_hometown)
+
+print(policy_number)
+
+# Lab 2
+
+# 1.a  Full list of reserved words
+
+# False, None, True, and, as, assert, break, class, continue, def, del, elif, 
+# else, except, finally, for, from, global, if, import, in, is, lambda, 
+# nonlocal, not, or, pass, raise, return, try, while, with, yield
+
+# b. 5 Definitions of Keywords
+
+# 1. if - used to make a ^^conditional statement^^. If the condition is True, then some action is performed.
+
+# 2. import - used to ^^import modules^^ to the project.
+
+# 3. while - used to start a ^^while loop^^. It continues iteration until a condition is no longer True.
+
+# 4. pass - It is a null statement in Python that will do nothing.
+
+# 5. def - used to define a function or a method in Python.
