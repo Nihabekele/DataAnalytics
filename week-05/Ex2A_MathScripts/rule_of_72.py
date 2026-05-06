6a9d6c301a2001d74bdcb45e8045d3220fa50ab2
Nihabekele <linabekele12@gmail.com>
2026-05-06 12:56:38 -0400

    "Add rule_of_72"

---
+++ week-05/Ex2A_MathScripts/rule_of_72.py
@@ -0,0 +1,15 @@
+# Define values
+savings = 5000.00
+interest_rate_decimal = 0.08  # 8% as a decimal
+interest_rate_whole = 8      # 8% as a whole number for the rule
+
+years_to_double = 72 / interest_rate_whole
+doubled_savings = savings * 2
+
+# results
+print("Your current savings is " + str(savings))
+
+# I Use .0% for the interest rate and .2f for the money
+# I Use .1f for the years as requested
+print("At a " + format(interest_rate_decimal, ".0%") + " interest rate, your savings account will be")
+print("worth " + format(doubled_savings, ".2f") + " in " + format(years_to_double, ".1f") + " years")
