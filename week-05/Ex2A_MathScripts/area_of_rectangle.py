33d22d0819df28f446034f1afedb46bcd1bbede2
Nihabekele <linabekele12@gmail.com>
2026-05-05 23:36:47 -0400

    "Add area_of_rectangle"

---
+++ week-05/Ex2A_MathScripts/area_of_rectangle.py
@@ -0,0 +1,13 @@
+# Side A is the birth month 
+side_a = 5
+
+# Side B is the day of the month 
+side_b = 21
+
+# Calculate the area
+area = side_a * side_b
+
+# Display the results
+print("Side A is " + str(side_a))
+print("Side B is " + str(side_b))
+print("The area of the rectangle is " + str(area))
