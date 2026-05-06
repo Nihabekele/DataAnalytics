51b0bc48dd8c6efa250b553773b05f8334121769
Nihabekele <linabekele12@gmail.com>
2026-05-05 23:27:22 -0400

    "Add Exercise 2A folder"

---
+++ week-05/Ex2A_MathScripts/net_worth.py
@@ -0,0 +1,19 @@
+# Lab 2
+
+# Define assets
+savings = 5000.00
+car_value = 15000.00
+total_assets = savings + car_value
+
+# Define debts
+credit_card = 1200.00
+student_loan = 8000.00
+total_debts = credit_card + student_loan
+
+# Calculate Net Worth
+net_worth = total_assets - total_debts
+
+# results
+print("Your total assets are " + format(total_assets, ".2f"))
+print("Your total debts are " + format(total_debts, ".2f"))
+print("Your net worth is " + format(net_worth, ".2f"))
